2e303ebc8c88eb98698c6705c42610716ea4db6a
nlair-dev <contact@nicolaslair.fr>
2021-08-21 19:21:00 +0200

    Structure MVVM et Binding de la Datagrid

--- Elephant_wpf/MainWindow.xaml.cs
+++ Elephant_wpf/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Forms;
 using ElephantLibrary;
+using Elephant_wpf.ViewModel;
 
 namespace Elephant
 {
@@ -10,19 +12,23 @@ namespace Elephant
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataFile Data;
+
         public MainWindow()
         {
-            
+            DataContext = new ViewModel();
+            //Data = new DataFile();
+            //DGridResult.ItemsSource = Data.Tags;
         }
 
         private void SearchTag_Click(object sender, RoutedEventArgs e)
         {
-            DGridResult.ItemsSource = DataFile.Search(txtName.Text);
+            DGridResult.ItemsSource = Data.Search(txtName.Text);
         }
 
         private void BtnUpdateData_Click(object sender, RoutedEventArgs e)
         {
-            DataFile.UpdateData();
+            Data.UpdateData();
         }
     }
 }
---
+++ Elephant_wpf/ViewModel/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using ElephantLibrary;
+using System.Linq;
+using System.Text;
+
+
+using System.Threading.Tasks;
+
+namespace Elephant_wpf.ViewModel
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ViewModel()
+        {
+            TagsList = new DataFile().Tags;
+        }
+
+        private ObservableCollection<TDCTag> _tagsList;
+        public ObservableCollection<TDCTag> TagsList
+        {
+            get => _tagsList;
+            set
+            {
+                _tagsList = value;
+                OnPropertyChanged(nameof(TagsList));
+            }
+        }
+    }
+}
